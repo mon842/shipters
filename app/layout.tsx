794edea86e0a9d2acc276d550f6c084538171991
mon842 <sayakbasak842@gmail.com>
2023-09-04 20:01:00 +0530

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -22,7 +22,7 @@ export default function RootLayout({
       <body className={`${inter.className} relative`}>
         <NavbarHead/>
         {children}
-        {/* <script src="https://cdnjs.cloudflare.com/ajax/libs/flowbite/1.6.5/flowbite.min.js"></script> */}
+        <script src="https://cdnjs.cloudflare.com/ajax/libs/flowbite/1.6.5/flowbite.min.js"></script>
       </body>
       
     </html>
